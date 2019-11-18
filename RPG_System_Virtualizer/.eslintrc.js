fb6ec33cac731fdaa2699a107acc85c1111e4f68
DaMasterHam <cmeisterham@gmail.com>
2019-11-18 19:28:38 +0100

    Adds environment configuration files
    Adds eslint config
    Adds vs code editor config
    Adds vuetify to plugins

---
+++ RPG_System_Virtualizer/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions:
+    {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended',
+        'plugin:vue/recommended'
+    ],
+    plugins: ['vue'],
+    rules:
+    {
+        "no-console": "off"
+    }
+}
--- RPG_System_Virtualizer/pages/index.vue
+++ RPG_System_Virtualizer/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>Yo!</h1>
+<div>
+    <h1>System Designer</h1>
+
+    <v-btn color="success">Woop!</v-btn>
+</div>
 </template>
+
+<script>
+
+</script>
+
+<style lang="stylus">
+
+</style>
