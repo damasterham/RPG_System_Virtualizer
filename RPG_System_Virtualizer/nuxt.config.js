70b3f67d0cf73a144eef24abe1ac8057da57c5e3
DaMasterHam <cmeisterham@gmail.com>
2019-11-21 16:57:44 +0100

    More configurartion for vuetify and css
    Adds more packages for nuxt & vuetfify and sets up configuration to work with css/sass
    Adds simple functionality to index
    Fixes .editorconfig to be in line with eslint

--- RPG_System_Virtualizer/nuxt.config.js
+++ RPG_System_Virtualizer/nuxt.config.js
@@ -1,5 +1,56 @@
+import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin'
+
 export default {
+  build: {
+    transpile: [
+      'vuetify/lib'
+    ],
     plugins: [
-        '@plugins/vuetify'
-    ]
+      new VuetifyLoaderPlugin()
+    ],
+    loaders: {
+      stylus: {
+        import: ['~assets/style/variables.styl']
+      }
+    },
+    extractCSS: true,
+    extend (config, ctx) {
+      config.node = {
+        fs: 'empty'
+      }
+
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+        // config.module.rules.push({
+        //   test: /\.s(c|a)ss$/,
+        //   use: [
+        //     'vue-style-loader',
+        //     'css-loader', {
+        //       loader: 'sass-loader',
+        //       options: {
+        //         implementaiton: require('sass'),
+        //         indentedSyntax: true
+        //       }
+        //     }
+        //   ]
+        // })
+      }
+    }
+  },
+  css: [
+    '@mdi/font/css/materialdesignicons.css',
+    // '@fortawesome/fontawesome-free/css/all.css',
+    'material-design-icons-iconfont/dist/material-design-icons.css'
+  ],
+  plugins: [
+    '@plugins/vuetify'
+  ]
+  // modules: [
+  //   ''
+  // ]
 }
--- RPG_System_Virtualizer/pages/index.vue
+++ RPG_System_Virtualizer/pages/index.vue
@@ -1,13 +1,25 @@
 <template>
-<div>
+<v-app dark>
     <h1>System Designer</h1>
 
-    <v-btn color="success">Woop!</v-btn>
-</div>
+    <v-btn color="success" @click="onClick" >{{text}}!</v-btn>
+</v-app>
 </template>
 
 <script>
-
+export default {
+  data () {
+    return {
+      text: 'Woop'
+    }
+  },
+  methods: {
+    onClick ()
+    {
+      this.text += 'Woop'
+    }
+  }
+}
 </script>
 
 <style lang="stylus">
