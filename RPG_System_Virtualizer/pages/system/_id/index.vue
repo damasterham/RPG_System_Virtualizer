ec8380ba6b4ff9fa28ca1cabc74bf829b0d1aa4a
Andreas Pedersen <andr4666@stud.kea.dk>
2019-12-11 14:22:52 +0100

    creates files for app flow
    WIP: system selection screen

---
+++ RPG_System_Virtualizer/pages/system/_id/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-container fluid fill-height>
+      <v-layout row wrap>
+        <v-flex v-for="system in systems" :key="system.id" xs3>
+          <v-card />
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import service from '~/plugins/feathers-service.js'
+export default {
+  computed: {
+    systems () {
+      return this.$store.getters['system/list']
+    }
+  },
+  created () {
+    service('systems')
+  },
+  mounted () {
+    this.$store.dispatch('systems/find', { query: { } })
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
