32011d93534844f717c14d1009990adf6121dd2f
Andreas Pedersen <andr4666@stud.kea.dk>
2020-01-16 19:44:09 +0100

    adds name and systemId to domainCollections

--- RPG_System_Virtualizer_Backend/src/models/domain_collections.model.js
+++ RPG_System_Virtualizer_Backend/src/models/domain_collections.model.js
@@ -7,6 +7,12 @@ module.exports = function (app) {
   const sequelizeClient = app.get('sequelize');
   const domainCollections = sequelizeClient.define('domain_collections', {
     // TODO hook hooks
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: '',
+      unique: 'collectionNameForSystem'
+    },
     version: {
       type: DataTypes.TEXT,
       allowNull: false,
@@ -23,6 +29,13 @@ module.exports = function (app) {
   // eslint-disable-next-line no-unused-vars
   domainCollections.associate = function (models) {
     // Define associations here
+    domainCollections.belongsTo(models.systems, {
+      foreignKey: {
+        name: 'systemId',
+        unique: 'collectionNameForSystem',
+        allowNull: false
+      }
+    });
     // See http://docs.sequelizejs.com/en/latest/docs/associations/
   };
 
