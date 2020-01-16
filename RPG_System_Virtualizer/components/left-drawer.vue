418cfe403d72d335e72797f66e10535130d345e3
Andreas Pedersen <andr4666@stud.kea.dk>
2020-01-16 21:01:18 +0100

    WIP: ElementCreator

--- RPG_System_Virtualizer/components/left-drawer.vue
+++ RPG_System_Virtualizer/components/left-drawer.vue
@@ -1,9 +1,13 @@
 <template>
   <v-navigation-drawer v-model="drawer" app>
-    <v-card width="100%" height="64px" @click="$router.push({ name: 'system-id' })">
-      <v-card-title class="display-1">
-        RPGSV
-      </v-card-title>
+    <v-card
+      class="text-center pa-4"
+      width="100%"
+      height="64px"
+      outlined
+      @click="$router.push({ name: 'system-id' })"
+    >
+      <span class="display-1">RPGSV</span>
     </v-card>
     <slot />
   </v-navigation-drawer>
--- RPG_System_Virtualizer/pages/system/_id/elementCreator.vue
+++ RPG_System_Virtualizer/pages/system/_id/elementCreator.vue
@@ -1,31 +1,59 @@
 <template>
   <v-app>
     <leftDrawer :drawer="domainDrawer">
-      <v-list shaped>
-        <template v-for="collection in domainCollections">
-          <v-list-tile
-            :key="collection.id"
-            :input-value="domainCollection !== null && collection.id === domainCollection.id"
-            color="blue-grey lighten-1"
-            @click="selectCollection(collection)"
-          >
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-list-item-title style="cursor: pointer" v-on="on">
-                  {{ collection.name }}
-                </v-list-item-title>
+      <template v-slot:default>
+        <v-tabs fixed-tabs>
+          <v-tab>
+            Collections
+          </v-tab>
+          <v-divider vertical />
+          <v-tooltip top open-delay="1000">
+            <template v-slot:activator="{ on }">
+              <v-tab v-on="on">
+                <span>Domains</span>
+              </v-tab>
+            </template>
+            <span>All domains marked with Instantiable are shown here.</span>
+          </v-tooltip>
+          <v-tab-item>
+            <v-divider />
+            <v-list shaped>
+              <template v-for="collection in domainCollections">
+                <v-list-tile
+                  :key="collection.id"
+                  :input-value="domainCollection !== null && collection.id === domainCollection.id"
+                  color="blue-grey lighten-1"
+                  @click="selectCollection(collection)"
+                >
+                  <v-tooltip right>
+                    <template v-slot:activator="{ on }">
+                      <v-list-item-title style="cursor: pointer" v-on="on">
+                        {{ collection.name }}
+                      </v-list-item-title>
+                    </template>
+                    <span>{{ collection.name }}</span>
+                  </v-tooltip>
+                </v-list-tile>
               </template>
-              <span>{{ collection.name }}</span>
-            </v-tooltip>
-          </v-list-tile>
-        </template>
-      </v-list>
+            </v-list>
+          </v-tab-item>
+          <v-tab-item>
+            <v-divider />
+            <v-list shaped />
+          </v-tab-item>
+        </v-tabs>
+      </template>
     </leftDrawer>
     <v-content>
       <appToolbar
         :title="system.name ? 'Content Creator - ' + system.name : 'Content Creator'"
         @toggleLeftDrawer="domainDrawer = !domainDrawer"
       />
+      <v-row>
+        <v-col />
+        <v-col />
+        <v-col />
+      </v-row>
     </v-content>
   </v-app>
 </template>
@@ -50,7 +78,12 @@ export default {
   },
   data () {
     return {
-      domainDrawer: true
+      domainDrawer: true,
+      sorting: 'A-Z',
+      sorts: [
+        'A-Z',
+        'Z-A'
+      ]
     }
   },
   computed: {
@@ -59,6 +92,12 @@ export default {
     },
     domainCollections () {
       return this.$store.getters['domain-collections/list']
+    },
+    domainCollectionInstances () {
+      const list = [...this.$store.getters['domain-collection-instances']].sort((a, b) => {
+        return this.sortAlphabetically(a, b, this.sorting === 'A-Z' ? 'ascending' : 'descending')
+      })
+      return list
     }
   },
   created () {
@@ -66,8 +105,19 @@ export default {
     service('domain-collections')(this.$store)
   },
   methods: {
-    selectCollection (collection) {
-      console.log(collection)
+    selectConcept (concept) {
+      console.log(concept)
+    },
+    sortAlphabetically (a, b, mode) {
+      if (a.name.toLowerCase() < b.name.toLowerCase()) { return mode === 'ascending' ? -1 : 1 }
+      if (a.name.toLowerCase() > b.name.toLowerCase()) { return mode === 'ascending' ? 1 : -1 }
+      return 0
+    },
+    assignSortAlgorithm (a, b) {
+      switch (this.sorting) {
+        case 'A-Z': return this.sortAlphabetically(a, b, 'ascending')
+        case 'Z-A': return this.sortAlphabetically(a, b, 'descending')
+      }
     }
   }
 }
